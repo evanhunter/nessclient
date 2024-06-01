79bc52c1ef43cb2737e2c3a30c45e9c19ff621c6
Evan Hunter <evan@ozhiker.com>
2024-06-01 10:43:28 +1000

    Add serial options to events and send-command scripts

--- nessclient/cli/events.py
+++ nessclient/cli/events.py
@@ -5,20 +5,30 @@ from ..client import Client
 from ..alarm import ArmingState, ArmingMode
 from ..event import BaseEvent
 from .server import DEFAULT_PORT
+from typing import Optional
 
 
 @click.command(help="Listen for emitted alarm events")
 @click.option("--host", default="localhost")
 @click.option("--port", type=int, default=DEFAULT_PORT)
+@click.option("--serial", type=str, default=None)
 @click.option("--update-interval", type=int, default=60)
 @click.option("--infer-arming-state/--no-infer-arming-state")
 def events(
-    host: str, port: int, update_interval: int, infer_arming_state: bool
+    host: Optional[str],
+    port: Optional[int],
+    serial: Optional[str],
+    update_interval: int,
+    infer_arming_state: bool,
 ) -> None:
+    if serial is not None and host == "localhost" and port == DEFAULT_PORT:
+        host = None
+        port = None
     loop = asyncio.get_event_loop()
     client = Client(
         host=host,
         port=port,
+        serial_tty=serial,
         infer_arming_state=infer_arming_state,
         update_interval=update_interval,
     )
